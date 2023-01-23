f2a887f3e2e9289b86878cf2944891e0a667c12d
moneyforey <moneyforey786@gmail.com>
2023-01-23 21:15:21 +0530

    update contact section

--- my-app/src/routes/contacts/contacts.jsx
+++ my-app/src/routes/contacts/contacts.jsx
@@ -1,33 +1,133 @@
-import { Box, Button, Flex,Stack, Text } from "@chakra-ui/react";
-import { ImMail2 } from "react-icons/im";
-import { IoCall } from "react-icons/io5";
-import styles from "./contact.module.css"
-
-const Contact =()=>{
-    return(
-        <Box my='100px' id="contacts">
-           <Text className={styles.name}>Contact me</Text>
-           
-                <Stack  className={styles.box2}>
-                   <Stack  border="1px solid black"  p={5}>
-                   <ImMail2/>
-                    
-                    <Text>moneyforey786@gmail.com</Text>
-                    <Button className={styles.btn} variant="outline" >
-                        <a href="mailto:moneyforey786@gmail.com">Send me a mail</a>
+import {
+    Container,
+    Flex,
+    Box,
+    Heading,
+    Text,
+    Button,
+    VStack,
+    Wrap,
+    WrapItem,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    Textarea,
+  } from '@chakra-ui/react';
+  import {
+    MdPhone,
+    MdEmail,
+    MdLocationOn,
+    MdOutlineEmail,
+  } from 'react-icons/md';
+  import { BsPerson } from 'react-icons/bs';
+  
+  export default function contact() {
+    return (
+      <Container  maxW="full" mt={0} id="contacts" centerContent overflow="hidden">
+        <Flex>
+          <Box
+            bg="#02054B"
+            color="white"
+            borderRadius="lg"
+            m={{ sm: 4, md: 16, lg: 10 }}
+            p={{ sm: 5, md: 5, lg: 16 }}>
+            <Box p={4}>
+              <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+                <WrapItem>
+                  <Box>
+                    <Heading>Contact</Heading>
+                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
+                      Fill up the form below to contact
+                    </Text>
+                    <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                      <VStack pl={0} spacing={3} alignItems="flex-start">
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="200px"
+                          variant="ghost"
+                          color="#DCE2FF"
+                          _hover={{ border: '2px solid #1C6FEB' }}
+                          leftIcon={<MdPhone color="#1970F1" size="20px" />}>
+                          +91-8094906950
                         </Button>
-                   </Stack>
-
-                       <Flex align="center" gap={2} border="1px solid black" p={5}>
-                       <IoCall />
-                    <Text>+91 8094906950</Text>
-                       </Flex>
-
-                </Stack>
-               
-
-        </Box>
-    )
-}
-
-export default Contact;
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="200px"
+                          variant="ghost"
+                          color="#DCE2FF"
+                          _hover={{ border: '2px solid #1C6FEB' }}
+                          leftIcon={<MdEmail color="#1970F1" size="20px" />}>
+                          moneyforey786@gmail.com
+                        </Button>
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="200px"
+                          variant="ghost"
+                          color="#DCE2FF"
+                          _hover={{ border: '2px solid #1C6FEB' }}
+                          leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
+                          Bharatpur, Rajasthan
+                        </Button>
+                      </VStack>
+                    </Box>
+                  </Box>
+                </WrapItem>
+                <WrapItem>
+                  <Box bg="white" borderRadius="lg">
+                    <Box m={8} color="#0B0E3F">
+                      <VStack spacing={5}>
+                        <FormControl id="name">
+                          <FormLabel>Your Name</FormLabel>
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<BsPerson color="gray.800" />}
+                            />
+                            <Input type="text" size="md" />
+                          </InputGroup>
+                        </FormControl>
+                        <FormControl id="name">
+                          <FormLabel>Mail</FormLabel>
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<MdOutlineEmail color="gray.800" />}
+                            />
+                            <Input type="text" size="md" />
+                          </InputGroup>
+                        </FormControl>
+                        <FormControl id="name">
+                          <FormLabel>Message</FormLabel>
+                          <Textarea
+                            borderColor="gray.300"
+                            _hover={{
+                              borderRadius: 'gray.300',
+                            }}
+                            placeholder="message"
+                          />
+                        </FormControl>
+                        <FormControl id="name" float="right">
+                          <Button
+                            variant="solid"
+                            bg="#0D74FF"
+                            color="white"
+                            _hover={{}}>
+                            Send Message
+                          </Button>
+                        </FormControl>
+                      </VStack>
+                    </Box>
+                  </Box>
+                </WrapItem>
+              </Wrap>
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
+    );
+  }
